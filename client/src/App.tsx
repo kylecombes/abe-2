0848569bfce48f9abd0c225aa03aa9d77480a0cb
Kyle Combes <kcombes@indigoag.com>
2021-01-31 15:34:53 -0500

    Create Snowpack React app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  return (
+    <div className="App">
+      Yay, an app!
+    </div>
+  );
+}
+
+export default App;
